7fbf969bb0007b6e692653f18066a72ed676eb1e
Camille Anelli <camilleanelli32@gmail.com>
2016-07-14 11:27:21 +0200

    add new and create actions

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,27 @@ class ItemsController < ApplicationController
   def index
     @todolist = Todolist.find(params[:todolist_id])
   end
+
+  def new
+    @todolist = Todolist.find(params[:todolist_id])
+    @item = @todolist.items.new
+  end
+
+  def create
+      @todolist = Todolist.find(params[:todolist_id])
+      @item = @todolist.items.new(params_items)
+      if @item.save
+        flash[:success] = "Produit bien ajouté!"
+        redirect_to todolist_items_path
+      else
+        flash[:error] = "Raté!"
+        render action: :new
+      end
+  end
+
+  private
+
+  def params_items
+    params[:item].permit(:content)
+  end
 end
