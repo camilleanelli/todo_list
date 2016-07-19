10c6e6ba9bc2a38dbf432eb7d5beb765aa46d8cf
Camille Anelli <camilleanelli32@gmail.com>
2016-07-19 11:34:19 +0200

    add scopes for items complete or incomplete

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,9 @@ class Item < ActiveRecord::Base
   validates :content, presence: true
   validates :content, length: { minimum: 2 }
 
+  scope :complete, -> { where("completed_at is not null") }
+  scope :incomplete, -> { where(completed_at: nil)}
+
   def completed
      !completed_at.blank?
   end
--- app/models/todolist.rb
+++ app/models/todolist.rb
@@ -5,4 +5,12 @@ class Todolist < ActiveRecord::Base
   validates :title, length: { minimum: 3}
   validates :description, presence: true
   validates :description, length: { minimum: 5}
+
+  def has_completed_items?
+    items.complete.size > 0
+  end
+
+  def has_incomplete_items?
+    items.incomplete.size > 0
+  end
 end
